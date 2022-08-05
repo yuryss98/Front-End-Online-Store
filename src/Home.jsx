79df30b6f85b4568f402871e4ff772822db42d98
Diogo Sales <diogosales.ads@gmail.com>
2022-08-05 17:33:34 -0300

    Requisito-4

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,24 @@
 // import PropTypes from 'prop-types'
 import React, { Component } from 'react';
+import { getCategories } from './services/api';
 
 export default class Home extends Component {
+  state = {
+    listCategory: [],
+  }
+
+  async componentDidMount() {
+    const result = await getCategories();
+    this.setState({
+      listCategory: result,
+    }, () => {
+      const { listCategory } = this.state;
+      console.log(listCategory);
+    });
+  }
+
   render() {
+    const { listCategory } = this.state;
     return (
       <div>
         <input type="text" name="" id="" />
@@ -12,6 +28,22 @@ export default class Home extends Component {
           Digite algum termo de pesquisa ou escolha uma categoria.
 
         </p>
+        {
+          listCategory && (
+            <div>
+              {listCategory.map(
+                (category) => (
+                  <div
+                    key={ category.id }
+                  >
+                    <p data-testid="category">
+                      {category.name}
+                    </p>
+                    <button type="button">Acessar</button>
+                  </div>),
+              )}
+            </div>)
+        }
       </div>
     );
   }
