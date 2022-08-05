764332e952fa0f1261d4727b0d17cce4099e29c9
Yury Stolarz Santana <yurysscareer@gmail.com>
2022-08-05 19:55:56 -0300

    requisito 5 grupo-10

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,30 +1,62 @@
 // import PropTypes from 'prop-types'
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { getCategories } from './services/api';
+import { getCategories, getProductsFromCategoryAndQuery } from './services/api';
 
 export default class Home extends Component {
   state = {
     listCategory: [],
+    search: '',
+    listProducts: '',
+    test: '',
   }
 
   async componentDidMount() {
     const result = await getCategories();
     this.setState({
       listCategory: result,
-    }, () => {
-      const { listCategory } = this.state;
-      console.log(listCategory);
     });
   }
 
-  // arrumando req 4
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  getProducts = async () => {
+    const { search } = this.state;
+    const response = await getProductsFromCategoryAndQuery('', search);
+    if (response.results.length !== 0) {
+      this.setState({
+        listProducts: response,
+      });
+    } else {
+      this.setState({
+        test: 'Nenhum produto foi encontrado',
+      });
+    }
+  }
 
   render() {
-    const { listCategory } = this.state;
+    const { listCategory, listProducts, test } = this.state;
     return (
       <div>
-        <input type="text" name="" id="" />
+        <input
+          type="text"
+          name="search"
+          data-testid="query-input"
+          onChange={ this.handleChange }
+        />
+        <button
+          data-testid="query-button"
+          type="button"
+          onClick={ this.getProducts }
+        >
+          Pesquisar
+
+        </button>
         <p
           data-testid="home-initial-message"
         >
@@ -47,6 +79,17 @@ export default class Home extends Component {
               )}
             </div>)
         }
+
+        {
+          listProducts ? listProducts.results.map((product) => (
+            <div key={ product.id } data-testid="product">
+              <p>{ product.title }</p>
+              <img src={ product.thumbnail } alt={ product.title } />
+              <p>{ product.price }</p>
+            </div>
+          )) : <p>{ test }</p>
+        }
+
       </div>
     );
   }
